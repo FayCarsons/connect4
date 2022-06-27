63935c470b01e3fefdc55d5ffebeab46512caa34
Fay Carsons <fay@Fays-MBP.fios-router.home>
2022-06-27 10:06:52 -0400

    kwikfix

--- sketch.js
+++ sketch.js
@@ -1,87 +1,112 @@
 const canvasSize = 400
 const gridSize = 8
 const sqr = canvasSize/gridSize
-let img;
-function preload() {
-  img = loadImage('transSmiley.png');
-  img2 = loadImage('transButterfly.png');
-} 
+let img
+
+let lowY  = 0
+
+let vbool = false
+let hbool = false
+let dlbool = false
+let drbool = false
+let turn = false
+let game = false
 
 var board = Array(gridSize).fill().map(() => Array(gridSize).fill(0)) 
 
 var x = 0
 var y = 0
 
-var turn = false
-var game = false
+function preload() {
+  img = loadImage('transSmiley.png')
+  img2 = loadImage('transButterfly.png')
+} 
 
 function setup() {
-  createCanvas(canvasSize, canvasSize);
-  background(0);
-  frameRate();  
+  createCanvas(canvasSize, canvasSize)
+  frameRate(10)
+  noLoop()
+  redraw()
 }
 
 function draw() {
   for (let y = 0; y < gridSize; y++){
     for (let x = 0; x < gridSize; x++){
-      fill((x+y)%2==0 ? 0 : 255);
-      rect(x*sqr, y*sqr, sqr, sqr);
+      fill((x+y)%2==0 ? 0 : 255)
+      rect(x*sqr, y*sqr, sqr, sqr)
       if (board[x][y] == 1) 
-      {image(img2,x*sqr,y*sqr,sqr,sqr)};
+      {image(img2,x*sqr,y*sqr,sqr,sqr)}
       if (board[x][y] == 2) 
-      {image(img,x*sqr,y*sqr,sqr,sqr)};      
+      {image(img,x*sqr,y*sqr,sqr,sqr)}    
     }
   }
 }
 
 function mouseClicked(){
-  place(int(mouseX/sqr), int(mouseY/sqr));
+  place(int(mouseX/sqr))
 }
 
-function place(gridIndexX,gridIndexY){
-  var lowY = 0
-  for (y = gridSize - 1; y != 0; y = y - 1) {
-    if (board[gridIndexX][y] == 0){
-      lowY = y;
-      break;
-    } 
+function place(gridIndexX) {  
+  if (!turn && !game) {
+    board[gridIndexX][lowestSpace(gridIndexX)] = 1
   }
-  if (!turn && !game) {board[gridIndexX][lowY] = 1};
-  if (turn && !game) {board[gridIndexX][lowY] = 2};
+  
+  if (turn && !game) {
+    board[gridIndexX][lowestSpace(gridIndexX)] = 2
+  }
 
-  verticalCheck();
-  horizontalCheck();
-  downRight();
-  downLeft();
+  turn = ! turn
 
-  turn = ! turn;
+  if (turn) {
+    place(int(random()*gridSize))
+  }
+    
+  if (!game) {redraw()}
+
+  verticalCheck()
+  horizontalCheck()
+  downRight()
+  downLeft()
+
+  if (game && !turn) {setTimeout(()=>alert("player 1 wins!",100))}
+  if (game && turn) {setTimeout(()=>alert("player 2 wins!",100))}
+}
+
+function lowestSpace(gridIndexX) {
+  lowY = 0
+  for (y = gridSize - 1; y != 0; y--) {
+    if (board[gridIndexX][y] == 0){
+      lowY = y
+      break  
+    }
+  }
+  return lowY 
 }
 
 function verticalCheck(){
   for (let x = 0; x < gridSize; x++){
     for (let y = 0; y < gridSize; y++){
-      let vbool = true;
+      vbool = true
       
       for (let z = 0; z < 4; z++) {
-        let player = turn ? 2 : 1;
-        vbool = vbool && (board[x][y+z] == player);
+        let player = turn ? 1 : 2
+        vbool = vbool && (board[x][y+z] == player)
       }
-      if (vbool) {game = true; gameWon()}
+      if (vbool) {game = true}
     } 
   } 
 }
 
 function horizontalCheck(){
-
   for (let x = 0; x < (gridSize-3); x++){
     for (let y = 0; y < gridSize; y++){
-      let hbool = true;
+      hbool = true
       
       for (let z = 0; z < 4; z++) {
-        let player = turn ? 2 : 1;
-        hbool = hbool && (board[x+z][y] == player);      
+        let player = turn ? 2 : 1
+        hbool = hbool && (board[x+z][y] == player)    
       }
-      if (hbool) {game = true; gameWon()}
+      if (hbool) {game = true}
     } 
   }  
 }
@@ -89,13 +114,13 @@ function horizontalCheck(){
 function downRight() {
   for (let x = 0; x < (gridSize-3); x++){
     for (let y = 0; y < (gridSize-3); y++){
-      let drbool = true;
+      drbool = true
 
       for (let z = 0; z < 4; z++) {
-        let player = turn ? 2 : 1;
-        drbool = drbool && (board[x+z][y+z] == player);      
+        let player = turn ? 2 : 1
+        drbool = drbool && (board[x+z][y+z] == player)    
       }
-      if (drbool) {game = true; gameWon()}
+      if (drbool) {game = true}
     } 
   }  
 }
@@ -103,18 +128,13 @@ function downRight() {
 function downLeft() {
   for (let x = 3; x < gridSize; x++){
     for (let y = 0; y < (gridSize-3); y++){
-      let dlbool = true;
+      dlbool = true
 
       for (let z = 0; z < 4; z++) {
-        let player = turn ? 2 : 1;
-        dlbool = dlbool && (board[x-z][y+z] == player);      
+        let player = turn ? 2 : 1
+        dlbool = dlbool && (board[x-z][y+z] == player)   
       }
-      if (dlbool) {game = true; gameWon()}
+      if (dlbool) {game = true}
     } 
   }  
-}
-
-function gameWon() {
-  if (game && !turn) {alert("player 1 wins!")}
-  if (game && turn) {alert("player 2 wins!")}
 }
